57ebc7782416bc53201c91fa3d64ba71fc36efe4
Avi Kapoor <avikapoor244@gmail.com>
2023-06-25 18:30:00 +0530

    okk
    Signed-off-by: Avi Kapoor <avikapoor244@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 function App() {
   const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
@@ -26,52 +27,52 @@ const calculator = document.querySelector(".calculator");
 const toggleIcon = document.querySelector(".toggler-icon");
   let isDark = true;
   
-themeToggleBtn.onclick = () => {
+  themeToggleBtn.onclick = () => {
   calculator.classList.toggle("dark");
   themeToggleBtn.classList.toggle("active");
   isDark = !isDark;
 }
   
   return (
-    <div class="container">
-        <div class="calculator dark">
-            <div class="theme-toggler active">
-                <i class="toggler-icon"></i>
+    <div className='container'>
+        <div className='calculator dark'>
+            <div className='theme-toggler active'>
+                <i class='toggler-icon'></i>
             </div>
-            <div class="display-screen">
-                <div id="display"></div>
+            <div className='display-screen'>
+                <div id='display'></div>
             </div>
-          <div class="buttons">
+          <div className='buttons'>
             <table>
             
                     <tr>
-                        <td><button class="btn-operator" id="clear">C</button></td>
-                        <td><button class="btn-operator" id="/">&divide;</button></td>
-                        <td><button class="btn-operator" id="*">&times;</button></td>
-                        <td><button class="btn-operator" id="backspace"> BS </button></td>
+                        <td><button className='btn-operator' id="clear">C</button></td>
+                        <td><button className= 'btn-operator' id="/">&divide;</button></td>
+                        <td><button className='btn-operator' id="*">&times;</button></td>
+                        <td><button className='btn-operator' id="backspace"> BS </button></td>
                     </tr>
                     <tr>
-                        <td><button class="btn-number" id="7">7</button></td>
-                        <td><button class="btn-number" id="8">8</button></td>
-                        <td><button class="btn-number" id="9">9</button></td>
-                        <td><button class="btn-operator" id="-">-</button></td>
+                        <td><button className='btn-number' id="7">7</button></td>
+                        <td><button className='btn-number' id="8">8</button></td>
+                        <td><button className='btn-number' id="9">9</button></td>
+                        <td><button className='btn-operator' id="-">-</button></td>
                     </tr>
                     <tr>
-                        <td><button class="btn-number" id="4">4</button></td>
-                        <td><button class="btn-number" id="5">5</button></td>
-                        <td><button class="btn-number" id="6">6</button></td>
-                        <td><button class="btn-operator" id="+">+</button></td>
+                        <td><button className='btn-number' id="4">4</button></td>
+                        <td><button className='btn-number' id="5">5</button></td>
+                        <td><button className='btn-number' id="6">6</button></td>
+                        <td><button className='btn-operator' id="+">+</button></td>
                     </tr>
                     <tr>
-                        <td><button class="btn-number" id="1">1</button></td>
-                        <td><button class="btn-number" id="2">2</button></td>
-                        <td><button class="btn-number" id="3">3</button></td>
-                        <td rowspan="2"><button class="btn-equal" id="equal">=</button></td>
+                        <td><button className='btn-number' id="1">1</button></td>
+                        <td><button className='btn-number' id="2">2</button></td>
+                        <td><button className='btn-number' id="3">3</button></td>
+                        <td rowspan="2"><button className='btn-equal' id="equal">=</button></td>
                     </tr>
                     <tr>
-                        <td><button class="btn-operator" id="(">(</button></td>
-                        <td><button class="btn-number" id="0">0</button></td>
-                        <td><button class="btn-operator" id=")">)</button></td>
+                        <td><button className='btn-operator' id="(">(</button></td>
+                        <td><button className='btn-number' id="0">0</button></td>
+                        <td><button className='btn-operator' id=")">)</button></td>
                     </tr>
                 
           </table>
